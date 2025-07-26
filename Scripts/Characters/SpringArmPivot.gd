8a58fbf860f657be7835f8ffaf4694b00c213c9b
duran55 <137329248+duran55@users.noreply.github.com>
2025-07-26 19:15:04 +0300

    add camera zoom support
    allow to change camera distance in-game

--- Scripts/Characters/SpringArmPivot.gd
+++ Scripts/Characters/SpringArmPivot.gd
@@ -10,15 +10,35 @@ const CAMERA_BLEND : float = 0.05
 @onready var spring_arm : SpringArm3D = $SpringArm3D
 @onready var camera : Camera3D = $SpringArm3D/Camera3D
 
+@export_group("Camera Zoom")
+@export var camera_distance_min := 1.0
+@export var camera_distance_max := 5.0
+@export var camera_distance_default := 3.0
+@export var camera_zoom_step = 0.2
+
+var mouse_sense = 0.003
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
+	camera.fov = normal_fov
+	spring_arm.spring_length = camera_distance_default
+	
+func _input(_event):
+	if Input.is_action_just_pressed("zoom_out"):
+		spring_arm.spring_length += camera_zoom_step
+		spring_arm.spring_length = clamp(spring_arm.spring_length, camera_distance_min, camera_distance_max)
+	if Input.is_action_just_pressed("zoom_in"):
+		spring_arm.spring_length -= camera_zoom_step
+		spring_arm.spring_length = clamp(spring_arm.spring_length, camera_distance_min, camera_distance_max)
+	if Input.is_action_just_pressed("zoom_reset"):
+		spring_arm.spring_length = camera_distance_default
+		
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * 0.005)
-		spring_arm.rotate_x(-event.relative.y * 0.005)
-		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -PI/4, PI/4)
-
+		rotate_y(-event.relative.x * mouse_sense)
+		spring_arm.rotate_x(-event.relative.y * mouse_sense)
+		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -PI/3, PI/3)
+		
 func _physics_process(_delta):
 	if change_fov_on_run:
 		if owner.is_on_floor():
